51ae94884065de290fbe9a779189d4b85ee9560f
melodykhounsavath <70538116+melodykhounsavath@users.noreply.github.com>
2020-11-01 22:14:05 -0900

    Added/implemented unit conversion

--- src/app.js
+++ src/app.js
@@ -37,6 +37,8 @@ function displayTemperature(response) {
     let iconElement = document.querySelector("#icon");
     iconElement.setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
     iconElement.setAttribute("alt", conditionElement.innerHTML = response.data.weather[0].description);
+
+    celsiusTemperature = response.data.main.temp;
 }
 
 function search(city) {
@@ -51,7 +53,35 @@ function handleSubmit(event) {
     search(locationInputElement.value)
 }
 
-search("Anchorage");
+function displayFahrenheitTemperature(event) {
+    event.preventDefault();
+     let temperatureElement = document.querySelector("#temperature")
+     celsiusLink.classList.remove("active");
+     fahrenheitLink.classList.add("active");
+    let fahrenheitTemperature = (celsiusTemperature * 9) / 5 + 32;
+    temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
+}
+
+function displayCelsiusTemperature(event) {
+    event.preventDefault();
+    celsiusLink.classList.add("active");
+    fahrenheitLink.classList.remove("active");
+    let temperatureElement = document.querySelector("#temperature")
+    temperatureElement.innerHTML = Math.round(celsiusTemperature);
+}
+
+
 
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", handleSubmit);
+
+let fahrenheitLink = document.querySelector("#fahrenheit-link");
+fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
+
+let celsiusLink = document.querySelector("#celsius-link");
+celsiusLink.addEventListener("click", displayCelsiusTemperature);
+
+let celsiusTemperature = null;
+
+
+search("Anchorage");
